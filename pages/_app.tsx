97bddb8b09e650e0285580b1e241cce7e92ae7be
moon <mmesyatz@gmail.com>
2021-01-18 23:07:30 +0200

    refactor: _app lang context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,30 +1,9 @@
 import { AppProps } from 'next/app';
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
-
-import context from '../src/context/context';
-
-import { LANGUAGES } from '../src/constants/languages';
 
 import '../styles/main.scss';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }): JSX.Element => {
-  const [language, setLanguage] = useState<'ru' | 'en'>('ru');
-  const router = useRouter();
-
-  useEffect(() => {
-    const curLang = Object.keys(LANGUAGES).find(
-      (key) => LANGUAGES[key].path === router.pathname
-    );
-
-    setLanguage(curLang as 'ru' | 'en');
-  }, [router.pathname]);
-
-  return (
-    <context.Provider value={{ language, setLanguage }}>
-      <Component {...pageProps} />
-    </context.Provider>
-  );
+  return <Component {...pageProps} />;
 };
 
 export default MyApp;
