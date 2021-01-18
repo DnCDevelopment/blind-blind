bc8ffe4a1281fa6e82fc69786f7658b948da62ea
Hladkko <hladkky@gmail.com>
2021-01-18 18:38:32 +0200

    fix: remove lang context

--- src/components/Header/MobileMenu.tsx
+++ src/components/Header/MobileMenu.tsx
@@ -3,27 +3,28 @@ import { useContext, useState } from 'react';
 import SearchInput from './SearchInput';
 import Sublist from './Sublist';
 
-import context from '../../context/context';
 import { indexContext } from '../../context/cockpitContext';
 
-import { IIndexContext, IContext } from '../../context/Types';
+import { IIndexContext } from '../../context/Types';
 import { IMobileMenuProps } from './Types';
 
 import { TRANSLATE } from '../../constants/languages';
 import { AboutListData } from '../../constants/header';
 
 import CrossSVG from '../../assets/svg/cross.svg';
+import { useRouter } from 'next/router';
 
 const MobileMenu: React.FC<IMobileMenuProps> = ({ close }) => {
   const [storeListOpen, setStoreListOpen] = useState<boolean>(false);
   const [aboutListOpen, setAboutListOpen] = useState<boolean>(false);
 
-  const { language } = useContext(context) as IContext;
+  const { locale } = useRouter();
+
   const { collectionsData, runwaysData, lookbooksData } = useContext(
     indexContext
   ) as IIndexContext;
 
-  const aboutSublist = AboutListData[language as 'en' | 'ru'];
+  const aboutSublist = AboutListData[locale as 'en' | 'ru'];
 
   aboutSublist[1].subsublist = lookbooksData;
   aboutSublist[2].subsublist = runwaysData;
@@ -40,7 +41,7 @@ const MobileMenu: React.FC<IMobileMenuProps> = ({ close }) => {
         </div>
         <SearchInput />
         <p role="presentation" onClick={() => setStoreListOpen(!storeListOpen)}>
-          {TRANSLATE[language as 'ru' | 'en'].store}
+          {TRANSLATE[locale as 'ru' | 'en'].store}
         </p>
         {storeListOpen && (
           <div className="mobile-menu__sublist mobile-menu__store-list-data-container">
@@ -48,7 +49,7 @@ const MobileMenu: React.FC<IMobileMenuProps> = ({ close }) => {
           </div>
         )}
         <p role="presentation" onClick={() => setAboutListOpen(!aboutListOpen)}>
-          {TRANSLATE[language as 'ru' | 'en'].aboutUs}
+          {TRANSLATE[locale as 'ru' | 'en'].aboutUs}
         </p>
         {aboutListOpen && (
           <div className="mobile-menu__sublist mobile-menu__about-list-data-container">
