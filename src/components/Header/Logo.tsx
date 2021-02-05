be20b7235f2c342795dd5e77390858beb8a021f0
Nivolves <ddubovik20@gmail.com>
2021-02-05 16:22:57 +0200

    chore: small changes

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -1,5 +1,13 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
 const Logo: React.FC = () => {
-  return <h1 className="logo">Blind-Blind</h1>;
+  const { locale } = useRouter();
+  return (
+    <Link href={locale === 'ru' ? '/' : '/ua'}>
+      <h1 className="logo">Blind-Blind</h1>
+    </Link>
+  );
 };
 
 export default Logo;
