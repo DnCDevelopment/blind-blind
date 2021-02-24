44100e5328d5d9c0ff3cecf74161f495ce0655e5
hladkky <60746298+hladkky@users.noreply.github.com>
2021-02-24 20:12:10 +0200

    BB-30

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,40 +4,55 @@ import MainCarousel from '../src/components/MainCarousel/MainCarousel';
 import Seo from '../src/components/Seo/Seo';
 
 import { IIndexPageProps } from '../src/pagesTypes';
-import { ICockpitCarousel, ICockpitGoodsRaw } from '../src/cockpitTypes';
+import {
+  ICockpitCarousel,
+  ICockpitCelebrityRaw,
+  ICockpitGoodsRaw,
+} from '../src/cockpitTypes';
 
 import { SEO_ITEMS } from '../src/constants/seoItems';
 
 import { getCockpitCollections } from '../src/utils/getCockpitData';
 import MainCollectionsSamples from '../src/components/MainCollectionsSamples/MainCollectionsSamples';
+import Celebrities from '../src/components/Celebrities/Celebrities';
 
-const IndexPage: NextPage<IIndexPageProps> = ({ carousel, goods, locale }) => (
-  <main className="main-page">
-    <Seo
-      title={SEO_ITEMS[locale].indexPage.title}
-      description={SEO_ITEMS[locale].indexPage.title}
-      breadcrumbs={[
-        {
-          title: SEO_ITEMS[locale].indexPage.breadcrumbName,
-          link: SEO_ITEMS[locale].indexPage.link,
-        },
-      ]}
-      lang={locale}
-      path={SEO_ITEMS[locale].indexPage.link}
-    />
-    <MainCarousel carousel={carousel}></MainCarousel>
-    <MainCollectionsSamples goods={goods} />
-  </main>
-);
+const IndexPage: NextPage<IIndexPageProps> = ({
+  carousel,
+  goods,
+  celebrities,
+  locale,
+}) => {
+  return (
+    <main className="main-page">
+      <Seo
+        title={SEO_ITEMS[locale].indexPage.title}
+        description={SEO_ITEMS[locale].indexPage.title}
+        breadcrumbs={[
+          {
+            title: SEO_ITEMS[locale].indexPage.breadcrumbName,
+            link: SEO_ITEMS[locale].indexPage.link,
+          },
+        ]}
+        lang={locale}
+        path={SEO_ITEMS[locale].indexPage.link}
+      />
+      <MainCarousel carousel={carousel} />
+      <MainCollectionsSamples goods={goods} />
+      <Celebrities celebrities={celebrities} />
+    </main>
+  );
+};
 
 export const getServerSideProps: GetServerSideProps = async ({
   locale,
   defaultLocale,
 }) => {
-  const collectionNames = ['mainCarousel', 'Goods'];
-  const [cockpitDataCarousel, cockpitDataGoods] = await getCockpitCollections(
-    collectionNames
-  );
+  const collectionNames = ['mainCarousel', 'Goods', 'Celebrities'];
+  const [
+    cockpitDataCarousel,
+    cockpitDataGoods,
+    cockpitDataCelebrities,
+  ] = await getCockpitCollections(collectionNames);
 
   const carousel = cockpitDataCarousel.entries.map((el: ICockpitCarousel) => {
     return {
@@ -61,8 +76,18 @@ export const getServerSideProps: GetServerSideProps = async ({
     };
   });
 
+  const celebrities = cockpitDataCelebrities.entries
+    .map((el: ICockpitCelebrityRaw) => {
+      return {
+        name: locale === defaultLocale ? el.name : el.name_en,
+        proffesion: locale === defaultLocale ? el.proffesion : el.proffesion_en,
+        photo: el.photo,
+      };
+    })
+    .slice(0, 3);
+
   return {
-    props: { carousel, goods, locale },
+    props: { carousel, goods, celebrities, locale },
   };
 };
 
