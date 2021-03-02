48e30df832db4f46525e7d28dde7adf7e5ee77d3
Nivolves <ddubovik20@gmail.com>
2021-03-02 20:59:02 +0200

    build: update image domain

--- next.config.js
+++ next.config.js
@@ -2,15 +2,15 @@ const withReactSvg = require('next-react-svg');
 const path = require('path');
 
 module.exports = withReactSvg({
-	images: {
-		domains: [ 'blind-blind-content.dncapp.website' ]
-	},
-	include: path.resolve(__dirname, 'src/assets/svg'),
-	webpack(config) {
-		return config;
-	},
-	i18n: {
+  images: {
+    domains: ['content.blind-blind.shop']
+  },
+  include: path.resolve(__dirname, 'src/assets/svg'),
+  webpack(config) {
+    return config;
+  },
+  i18n: {
     locales: ['ru', 'en'],
-		defaultLocale: 'ru',
-	}
+    defaultLocale: 'ru',
+  }
 });
