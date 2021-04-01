4b93afb35658a9cd7266c715ab5abbd1ffb11049
Nivolves <ddubovik20@gmail.com>
2021-04-01 17:10:46 +0300

    chore: centered logo

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -1,11 +1,17 @@
+import { useContext } from 'react';
 import Link from 'next/link';
 
 import LogoSvg from '../../assets/svg/logo.svg';
 
+import { ICartContext } from '../../context/Types';
+
+import { cartContext } from '../../context/cartContext';
+
 const Logo: React.FC = () => {
+  const { cart } = useContext(cartContext) as ICartContext;
   return (
     <Link href="/">
-      <a>
+      <a className={cart.length ? 'not-empty' : 'empty'} style={{}}>
         <LogoSvg width={130} />
       </a>
     </Link>
