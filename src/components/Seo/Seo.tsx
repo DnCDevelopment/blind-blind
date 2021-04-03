fd76315a94b1123d2dccd540bf72b4bf7626d4b5
Nivolves <ddubovik20@gmail.com>
2021-04-03 20:33:34 +0300

    add: favicons

--- src/components/Seo/Seo.tsx
+++ src/components/Seo/Seo.tsx
@@ -76,7 +76,24 @@ const Seo: React.FC<ISeoProps> = ({
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta name="og:description" content={description} />
-      <link rel="shortcut icon" href="/favicon.jpg"></link>
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
       <meta property="og:title" content={title} />
       <meta property="og:type" content="website" />
       <meta property="og:url" content={path} />
