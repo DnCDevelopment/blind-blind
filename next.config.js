4b565bdf38291ee92cf22d887f614c7cb44e8da4
Batya0tvoy <alkashvalera@ukr.net>
2024-12-09 23:00:01 +0200

    optimized image size

--- next.config.js
+++ next.config.js
@@ -2,17 +2,18 @@ const withReactSvg = require('next-react-svg');
 const path = require('path');
 
 module.exports = withReactSvg({
-    images: {
-        deviceSizes: [1800],
-        domains: ['content.blind-blind.com'],
-    },
-    include: path.resolve(__dirname, 'src/assets/svg'),
-    webpack(config) {
-        return config;
-    },
-    i18n: {
-        locales: ['ru', 'en'],
-        defaultLocale: 'ru',
-        localeDetection: false,
-    },
+  images: {
+    deviceSizes: [1200],
+    domains: ['content.blind-blind.com'],
+    formats: ['image/avif', 'image/webp'],
+  },
+  include: path.resolve(__dirname, 'src/assets/svg'),
+  webpack(config) {
+    return config;
+  },
+  i18n: {
+    locales: ['ru', 'en'],
+    defaultLocale: 'ru',
+    localeDetection: false,
+  },
 });
